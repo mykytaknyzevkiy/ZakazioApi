531687079a5978a152b2d8b83936911f194b0321
nikita <neka1dev@gmail.com>
2020-11-13 17:48:33 +0200

    Created feedback entity

---
+++ src/main/kotlin/com/zakaion/api/entity/FeedbackEntity.kt
@@ -0,0 +1,17 @@
+package com.zakaion.api.entity
+
+import com.zakaion.api.entity.user.UserEntity
+import javax.persistence.*
+
+@Entity
+data class FeedbackEntity(
+        @Id
+        @GeneratedValue(strategy= GenerationType.AUTO)
+        val id: Long = 0L,
+        @OneToOne
+        val order: OrderEntity,
+        @OneToOne
+        val user: UserEntity,
+        val stars: Int,
+        val comments: String
+)
