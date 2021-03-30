728b93ac4c5feb9e1a4569ca7157794df94f4413
mykyta <nikita.knyazevkiy@wisercat.eu>
2021-03-30 13:52:09 +0300

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        maven { url = uri("https://repo.spring.io/milestone") }
+        maven { url = uri("https://repo.spring.io/snapshot") }
+        gradlePluginPortal()
+    }
+}
+rootProject.name = "Zazakio-api"
+
+include("api")
+include("socket")
