ed64585a03af9f06df35392e3eb0b82dcd135866
mykyta <nikita.knyazevkiy@wisercat.eu>
2021-04-02 13:30:10 +0300

    fix sms url 2

--- api/src/main/kotlin/com/zakaion/api/service/SmsService.kt
+++ api/src/main/kotlin/com/zakaion/api/service/SmsService.kt
@@ -20,7 +20,7 @@ class SmsService(restTemplateBuilder: RestTemplateBuilder) {
     }
 
     fun sendMsg(phoneNumber: String, msg: String) {
-        val url = "http://sms:8282/api/v1/message/add"
+        val url = "https://sms.zakazy.online/api/v1/message/add"
 
         // create a map for post parameters
         val map = HashMap<String, Any>()
