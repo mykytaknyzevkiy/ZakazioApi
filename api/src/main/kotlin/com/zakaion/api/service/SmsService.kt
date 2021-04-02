9f0785cc30dcf83f6a8e189d78d55de2ab62d5b2
mykyta <nikita.knyazevkiy@wisercat.eu>
2021-04-02 13:10:13 +0300

    Implement sms service to api

--- api/src/main/kotlin/com/zakaion/api/service/SmsService.kt
+++ api/src/main/kotlin/com/zakaion/api/service/SmsService.kt
@@ -1,12 +1,38 @@
 package com.zakaion.api.service
 
+import com.google.gson.Gson
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.client.support.BasicAuthorizationInterceptor
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-class SmsService {
+class SmsService(restTemplateBuilder: RestTemplateBuilder) {
+
+    private val restTemplate = restTemplateBuilder.build()
+
+    private val headers = HttpHeaders().apply {
+        contentType = MediaType.APPLICATION_JSON
+        accept = Collections.singletonList(MediaType.APPLICATION_JSON)
+    }
 
     fun sendMsg(phoneNumber: String, msg: String) {
-        //TODO(Send sms)
+        val url = "http://sms:8282/message/add"
+
+        // create a map for post parameters
+        val map = HashMap<String, Any>()
+        map["phoneNumber"] = phoneNumber
+        map["content"] = msg
+
+        println(Gson().toJson(map))
+
+        // build the request
+        val entity: HttpEntity<Map<String, Any>> = HttpEntity(map, headers)
+
+        restTemplate.postForObject(url, entity, String::class.java)
     }
 
     fun sendCode(phoneNumber: String, code: String) {
