f920f80ceb0652f3d92baf58b5918117338ac0bf
mykyta <nikita.knyazevkiy@wisercat.eu>
2021-04-11 02:43:59 +0300

    Bugs

--- api/src/main/kotlin/com/zakaion/api/factor/user/FullOrderClientFactor.kt
+++ api/src/main/kotlin/com/zakaion/api/factor/user/FullOrderClientFactor.kt
@@ -58,13 +58,13 @@ class FullOrderClientFactor(user: UserEntity,
                 passport = passportDao.findAll().find { it.user.id == id },
                 portfolioCount = portfolio.size
         ).apply {
-            if (order.enable) {
-                order.enable =
-                    isEmailActive &&
-                            isPassportActive &&
-                            portfolio.isNotEmpty() &&
-                            !isBlocked && city != null
-            }
+            order.enable =
+                        isEmailActive &&
+                        isPassportActive &&
+                        portfolio.isNotEmpty() &&
+                        !isBlocked &&
+                        city != null &&
+                        order.count.open <= order.count.max
             if (!order.enable && !isBlocked)
                 status = UserStatus.PROCESS
         }
