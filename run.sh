6a4b6fd17d64569745ad86db0ead5d2a63adf6ff
mykyta <nikita.knyzevskiy@gmail.com>
2021-04-17 02:20:36 +0300

    run script w crm

--- run.sh
+++ run.sh
@@ -2,6 +2,13 @@ git reset --hard
 git clean -d -f
 git pull
 
+# shellcheck disable=SC2164
+cd crm
+
+flutter build web --no-sound-null-safety --web-renderer canvaskit
+
+cd /root/ZakazioApi
+
 chmod a+x ./gradlew
 
 ./gradlew clean
