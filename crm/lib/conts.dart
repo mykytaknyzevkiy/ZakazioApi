82cd731d5905534c0618922a606999eb540d2873
mykyta <nikita.knyzevskiy@gmail.com>
2021-04-17 21:38:42 +0300

    order help message

--- crm/lib/conts.dart
+++ crm/lib/conts.dart
@@ -5,7 +5,7 @@ const accentColor = Colors.white;
 
 const cardBackgroundColor = Colors.white;
 
-const appName = "Zakazy-CRM V2.67";
+const appName = "Zakazy-CRM V2.68";
 
 const appUrl = "https://api.zakazy.online/api/v1";
 
