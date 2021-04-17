bae00bb9c5178ed4f197812eca837f83dbd6d41b
mykyta <nikita.knyzevskiy@gmail.com>
2021-04-17 18:48:19 +0300

    webserver config 5

--- run.sh
+++ run.sh
@@ -2,12 +2,7 @@ git reset --hard
 git clean -d -f
 git pull
 
-# shellcheck disable=SC2164
-cd crm
 
-~/development/flutter/bin/flutter build web --no-sound-null-safety --web-renderer canvaskit
-
-cd /root/ZakazioApi
 
 chmod a+x ./gradlew
 
