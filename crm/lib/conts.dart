a46c3bd3783cb19c5374f6ee84872f3e4db6d011
mykyta <nikita.knyzevskiy@gmail.com>
2021-04-17 15:45:49 +0300

    version up

--- crm/lib/conts.dart
+++ crm/lib/conts.dart
@@ -5,7 +5,7 @@ const accentColor = Colors.white;
 
 const cardBackgroundColor = Colors.white;
 
-const appName = "Zakazy-CRM V2.65";
+const appName = "Zakazy-CRM V2.66";
 
 const appUrl = "https://api.zakazy.online/api/v1";
 
