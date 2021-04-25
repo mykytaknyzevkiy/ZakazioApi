333c11774cbe3c2a789c4d149d8b62860675fc64
mykyta <nikita.knyzevskiy@gmail.com>
2021-04-25 18:24:07 +0300

    analytic

--- open/components/TheFooter.vue
+++ open/components/TheFooter.vue
@@ -41,9 +41,10 @@
           </a>
           <a
             href="https://file.zakazy.online/document/%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0-%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%BD%D0%B4%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8%20(1).pdf"
-            >Политика обработки персональных данных</a
-          >
-          <a href="https://file.zakazy.online/document/%D0%BE%D1%82%D0%BA%D0%B0%D0%B7_%D0%BE%D1%82_%D0%BE%D1%82%D0%B2%D0%B5%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D0%B8_%D0%B7%D0%B0_%D0%BF%D1%80%D0%B8%D0%B1%D1%8B%D0%BB%D1%8C.pdf">Отказ от ответственности за прибыль</a>
+            >Политика обработки персональных данных</a>
+          <a
+            href="https://file.zakazy.online/document/%D0%BE%D1%82%D0%BA%D0%B0%D0%B7_%D0%BE%D1%82_%D0%BE%D1%82%D0%B2%D0%B5%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D0%B8_%D0%B7%D0%B0_%D0%BF%D1%80%D0%B8%D0%B1%D1%8B%D0%BB%D1%8C.pdf">
+            Отказ от ответственности за прибыль</a>
         </div>
       </div>
       <div class="footer__col">
