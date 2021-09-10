d7605d7aa6105f3481c3eb9c4c7f6fd3640fbb08
mykyta <nikita.knyzevskiy@gmail.com>
2021-09-11 02:00:14 +0300

    fix: something

--- open/components/TheFooter.vue
+++ open/components/TheFooter.vue
@@ -36,25 +36,24 @@
       <div class="footer__col">
         <div class="footer__links">
           <a
-            href="https://file.zakazy.online/document/%D0%A3%D1%81%D0%BB%D0%BE%D0%B2%D0%B8%D1%8F%20%D0%B8%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(%D0%9E%D1%84%D0%B5%D1%80%D1%82%D0%B0).pdf"
-            >Условия использования (Оферта)
+            href="https://file.zakazy.online/document/Пользовательское соглашение.pdf"
+            >Пользовательское соглашение
           </a>
           <a
-            href="https://file.zakazy.online/document/%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0-%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%BD%D0%B4%D0%B5%D0%BD%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8%20(1).pdf"
-            >Политика обработки персональных данных</a
+            href="https://file.zakazy.online/document/Оферта_между_Партнером_и_Пользователем.pdf"
+            >Оферта между Партнером и Пользователем</a
           >
           <a
-            href="https://file.zakazy.online/document/%D0%BE%D1%82%D0%BA%D0%B0%D0%B7_%D0%BE%D1%82_%D0%BE%D1%82%D0%B2%D0%B5%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D0%B8_%D0%B7%D0%B0_%D0%BF%D1%80%D0%B8%D0%B1%D1%8B%D0%BB%D1%8C.pdf"
+            href="https://file.zakazy.online/document/Политика  конфиденциальности.pdf"
           >
-            Отказ от ответственности за прибыль</a
+            Политика конфиденциальности</a
           >
+          <a href="https://file.zakazy.online/document/Оферта БС.pdf">Оферта БС</a>
         </div>
       </div>
       <div class="footer__col">
-        <div class="footer__contact">
-          <a :href="`tel:+${contacts.phoneNumber}`" class="footer__phone">{{
-            contacts.phoneNumber
-          }}</a>
+        <div class="footer__visa">
+          <img src="https://file.zakazy.online/document/2021-09-11%2001.56.03.jpg" style="width: 100px;"/>
         </div>
       </div>
       <div class="footer__col">
